ba21e6d9375797d058c7765234b80d449f91bd97
Andrea Nassif <andrea.e.nassif@gmail.com>
2022-09-18 23:20:28 -0300

    add ptos 3 y 4

--- archivo.js
+++ archivo.js
@@ -5,9 +5,51 @@ class Usuario {
         this.libros = libros;
         this.mascotas = mascotas;
     }
+
+    addBook(nombre, autor){
+        const newBook = {nombre: nombre, autor: autor};
+        console.log(newBook);
+        this.libros.push(newBook);
+    }
+
+    getFullName () {
+        return `${this.nombre} + ${this.apellido}`;
+        
+    }
+
+    addMascota(mascotas){
+        const newMascota = {mascotas: mascotas};
+        console.log(newMascota);
+        this.mascotas.push(newMascota);
+    }
+
+    countMascotas(){
+        this.mascotas.length();
+    }
+
+    getBookNames(){ 
+        this.libros.map();
+    }
+
+    
 }
 
 
 
-const user1 = new Usuario("user1", "nassif1", "libro1", "perro");
+
+const user1 = new Usuario("user1", "nassif1", [{nombre:"libro1", autor:"author1"}], ["perro", "gato"]);
 console.log(user1);
+user1.addBook("libro2", "author2");
+user1.getFullName();
+user1.addMascota("perico");
+user1.getBookNames();
+user1.countMascotas();
+
+
+
+
+
+
+
+
+
