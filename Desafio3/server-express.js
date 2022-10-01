2ffc3e224a0aea9f965ba4d4f40c7e167295f4f0
Andrea Nassif <andrea.e.nassif@gmail.com>
2022-10-01 12:22:27 -0300

    desafio3-server

--- Desafio3/server-express.js
+++ Desafio3/server-express.js
@@ -5,13 +5,19 @@ const express = require('express');
 const app = express();
 const listProducts = new Contenedor("./products.txt")
 
-app.get("/products",(req, res)=>{
-    res.send(`Los productos son: ${listProducts.getAll()} `)   
+
+app.get("/products", async (req, res)=>{
+    
+    res.json(await listProducts.getAll())
+
 })
 
-app.get("/productRandom",(req, res)=>{
-
-    res.send(`${listProducts.getById()}`)
+app.get("/productRandom", async (req, res)=>{
+    const allProducts = listProducts.getAll()
+    let numRandom = Math.round(Math.random(1,allProducts.length)*10+1)
+    console.log(numRandom)
+    const idRandom = await listProducts.getById(numRandom)
+    res.send(idRandom)
 })
 
 app.listen (8080, ()=>{
